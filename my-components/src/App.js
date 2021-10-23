516ffd070c0e8c3de9effcb7d951744c6571af9d
Sunny <sunnygb.8@gmail.com>
2021-10-23 01:29:19 +0000

    Enable live reload for React inside of dev container

--- my-components/src/App.js
+++ my-components/src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <br />
+          Hello React World!
+        </p>        
         <a
           className="App-link"
           href="https://reactjs.org"
