f833f1c9b47ce1e00643560a49ef06e28459d4a0
Sunny <sunnygb.8@gmail.com>
2021-10-24 11:56:48 +0000

    Passing multiple props

--- my-components/src/App.js
+++ my-components/src/App.js
@@ -1,11 +1,27 @@
 import CommentDetail from './CommentDetail';
+import faker from 'faker'
 
 function App() {
   return (
     <div className="ui container comments">      
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail 
+        author="Sam" 
+        timePosted="Today at 4:45PM" 
+        comment="Buy One, Take One Pizza starting now!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Arlene" 
+        timePosted="Today at 2:00AM" 
+        comment="Fabulous." 
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Jane" 
+        timePosted="Yesterday at 5:00PM" 
+        comment="Yummmmmmyy!!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 }
