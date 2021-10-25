7580cb080d31989345675a530c10ec677e7d6948
Sunny <sunnygb.8@gmail.com>
2021-10-25 07:00:32 +0000

    Test: Children Through Props

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,4 +1,5 @@
 import Message from './exercises/Ex1Message'
+import Placeholder from './exercises/Ex2Placeholder';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
 export default App;
 
 //- Exercises
-function Quiz(ex) {  
+function Quiz(ex) {
   switch (ex) {
     case 1:
       return (
@@ -19,14 +20,34 @@ function Quiz(ex) {
           title="Changes in Service"
           message="We just updated our privacy policy!!"
         />
-      )      
+      )
+
+    case 2:
+      return (
+        <div>
+          <Placeholder>
+            <div className="ui icon header">
+              <i className="pdf file outline icon"></i>
+              No documents are listed for this computer.
+            </div>
+            <div className="ui primary button">Add Document</div>
+          </Placeholder>
+          <Placeholder>
+            <h4 className="ui header">For your information</h4>
+            <p>
+              The house was located at the top of the hill at the end of a winding road.
+              It wasn't obvious that the house was there, but everyone in town knew that it existed. They were just all too afraid to ever go and see it in person.
+            </p>
+          </Placeholder>
+        </div>
+      )
 
     default:
-        console.log('Out of ex ' + ex + '.')
-        return (
-          <div>Hello React Components!</div>
-        )
+      console.log('Out of ex ' + ex + '.')
+      return (
+        <div>Hello React Components!</div>
+      )
   }
 }
 
-const ex = 1;
+const ex = 2;
---
+++ quiz/src/exercises/Ex2Placeholder.js
@@ -0,0 +1,9 @@
+const Placeholder = (props) => {
+    return (
+        <div className="ui placeholder segment">
+            {props.children}
+        </div>
+    )
+}
+
+export default Placeholder
