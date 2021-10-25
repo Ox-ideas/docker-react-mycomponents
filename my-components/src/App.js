85556cf016f0bb38da01ea78c74c4186acd3de47
Sunny <sunnygb.8@gmail.com>
2021-10-25 00:56:21 +0000

    Passing child component as props

--- my-components/src/App.js
+++ my-components/src/App.js
@@ -1,28 +1,36 @@
-import CommentDetail from './CommentDetail';
 import faker from 'faker'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 function App() {
   return (
-    <div className="ui container comments">      
-      <CommentDetail 
-        author="Sam" 
-        timePosted="Today at 4:45PM" 
-        comment="Buy One, Take One Pizza starting now!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Arlene" 
-        timePosted="Today at 2:00AM" 
-        comment="Fabulous." 
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Jane" 
-        timePosted="Yesterday at 5:00PM" 
-        comment="Yummmmmmyy!!"
-        avatar={faker.image.avatar()}
-      />
-    </div>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timePosted="Today at 4:45PM"
+          comment="Buy One, Take One Pizza starting now!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard >
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Arlene"
+          timePosted="Today at 2:00AM"
+          comment="Fabulous."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timePosted="Yesterday at 5:00PM"
+          comment="Yummmmmmyy!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div >
   );
 }
 
