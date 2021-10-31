3ed97d5206ea0b24a34120bab04e260e1af528ac
Sunny <sunnygb.8@gmail.com>
2021-10-31 10:06:34 +0000

    Don't place calls that takes some time to process inside a render()

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,14 +5,13 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = { lat: null }
-  }
-
-  render () {
     navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       err => console.log(err)
     )
-    
+  }
+
+  render () {
     return (
       <div>Latitude: {this.state.lat}</div>
     )
