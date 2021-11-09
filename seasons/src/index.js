09483ae0ef24b8fa6f433370e0bc698b5d41c410
Sunny <sunnygb.8@gmail.com>
2021-11-09 10:16:11 +0000

    Introducing life cycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,6 +17,14 @@ class App extends React.Component {
     )
   }
 
+  componentDidMount() {
+    console.log('My component was rendered to the screen.')
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!')
+  }
+
   render () {
     if (this.state.errorMsg && !this.state.lat) {
       return <div>Error: {this.state.errorMsg}</div>
