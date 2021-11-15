b9a3a073d685e9f8ba261cef65259346c1aabaaf
Sunny <sunnygb.8@gmail.com>
2021-11-15 08:08:42 +0000

    Showing Icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -9,11 +9,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth())
     const text = season === 'winter' ? 'Burr, it\'s chilly' : 'Let\'s hit the beach'
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
     console.log(season)
     
     return (
         <div>
-            {text}
+            <i className={`${icon} icon`} />
+            <h3>{text}</h3>
+            <i className={`${icon} icon`} />
         </div>
     )
 }
