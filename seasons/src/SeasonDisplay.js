be2868a2da0f57b51ba3fe85d982a5aed6df220f
Sunny <sunnygb.8@gmail.com>
2021-11-16 04:45:04 +0000

    Extracting Options to Config Objects

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,3 +1,14 @@
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun',
+    },
+    winter: {
+        text: "Burr it's cold!",
+        iconName: 'snowflake',
+    },
+}
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter'
@@ -8,15 +19,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth())
-    const text = season === 'winter' ? 'Burr, it\'s chilly' : 'Let\'s hit the beach'
-    const icon = season === 'winter' ? 'snowflake' : 'sun'
+    const { text, iconName } = seasonConfig[season]
     console.log(season)
     
     return (
         <div>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
             <h3>{text}</h3>
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
         </div>
     )
 }
