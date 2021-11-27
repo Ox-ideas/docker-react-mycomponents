c21788256a31232270f99c822e8a68afe7b7b18c
Sunny <sunnygb.8@gmail.com>
2021-11-27 23:03:28 +0000

    Specifying default props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -26,7 +26,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <Spinner />
+    return <Spinner msg="Please accept location request" />
   }
 }
 
